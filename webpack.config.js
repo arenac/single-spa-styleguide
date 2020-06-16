d1952ebc37a8b22b2b8080a541b06eca9e17924a
Nilo Neregato <neregato.nilo@gmail.com>
2020-06-16 23:16:35 +0200

    fix loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,40 @@
 const webpackMerge = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa");
 
-module.exports = (webpackConfigEnv) => {
+module.exports = webpackConfigEnv => {
   const defaultConfig = singleSpaDefaults({
     orgName: "mfe",
     projectName: "global-style",
-    webpackConfigEnv,
+    webpackConfigEnv
   });
 
   return webpackMerge.smart(defaultConfig, {
-    // modify the webpack config however you'd like to by adding to this object
+    module: {
+      rules: [
+        {
+          test: /\.s(c|a)ss$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: "style-loader"
+          },
+        },
+        {
+          test: /\.s(c|a)ss$/,
+          use: { 
+            loader: "css-loader"
+          },
+        },
+        {
+          test: /\.s(c|a)ss$/,
+          use: {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              plugins: [require("tailwindcss"), require("autoprefixer")]
+            },
+          },
+        }
+      ]
+    }
   });
 };
